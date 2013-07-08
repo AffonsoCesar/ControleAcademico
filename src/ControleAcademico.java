6461d82114aa386084ebbb596df18c7410b878c3
Marcelo Phillip antos <marcelo.santos@dce.ufpb.br>
2013-07-08 13:10:56 -0300

    Modificado 008/07/2013 às 13:10

--- src/ControleAcademico.java
+++ src/ControleAcademico.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class ControleAcademico {
-	private List<Aluno> alunos = new ArrayList<>();
+	private List<Aluno> alunos = new ArrayList<Aluno>();
 	
 	public void setPrimeiroAluno(Aluno alunos) {
 		
